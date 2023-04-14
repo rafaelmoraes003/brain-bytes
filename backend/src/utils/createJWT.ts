e0606ce3386cf86e7c4169d43b288123bff81db8
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 18:58:23 -0300

    fix: payload jwt sign

--- backend/src/utils/createJWT.ts
+++ backend/src/utils/createJWT.ts
@@ -1,10 +1,12 @@
 import jwt from 'jsonwebtoken';
 import IUser from '../interfaces/IUser';
+import 'dotenv/config';
 
 const JWT_SECRET: string = process.env.JWT_SECRET as string;
 
 const createJWT = (user: IUser): string => {
-  const token: string = jwt.sign(user, JWT_SECRET, { algorithm: 'HS256' });
+  const { username, password } = user;
+  const token: string = jwt.sign({ username, password }, JWT_SECRET, { algorithm: 'HS256' });
   return token;
 };
 

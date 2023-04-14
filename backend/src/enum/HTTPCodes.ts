16c5376d280d835b1ac5394efec54874c785e917
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 18:21:35 -0300

    feat: enum de códigos de status http

---
+++ backend/src/enum/HTTPCodes.ts
@@ -0,0 +1,11 @@
+enum HTTPCodes {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  SEMANTIC_ERROR = 422,
+  SERVER_ERROR = 500,
+}
+
+export default HTTPCodes;

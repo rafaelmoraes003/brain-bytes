b33533f246535f6377e35480b506a61d8b7d4c25
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 18:22:04 -0300

    feat[App]: implementa método setApplicationRoutes, que define rotas da aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import HTTPCodes from './enum/HTTPCodes';
 
 class App {
   public app: Express;
@@ -12,6 +13,13 @@ class App {
   private config(): void {
     this.app.use(express.json());
     this.app.use(cors());
+    this.setApplicationRoutes();
+  }
+
+  private setApplicationRoutes(): void {
+    this.app.get('/ping', (_req: Request, res: Response): void => {
+      res.status(HTTPCodes.OK).json({ pong: true });
+    });
   }
 
 }

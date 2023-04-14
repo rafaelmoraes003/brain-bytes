dc3c10d59a3738487b50fe1b4c49a8888cdc2e25
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 18:19:50 -0300

    feat[App]: implementa método config, que configura body parser e cors na aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,17 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 
 class App {
   public app: Express;
 
   constructor() {
     this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    this.app.use(express.json());
+    this.app.use(cors());
   }
 
 }

d1c45fa92b7aae02d3feb650d07a399912b979a4
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 23:37:21 -0300

    feat: função para validar objectid

---
+++ backend/src/utils/validateObjectId.ts
@@ -0,0 +1,14 @@
+import { isValidObjectId } from 'mongoose';
+import CustomError from './CustomError';
+import HTTPCodes from '../enum/HTTPCodes';
+
+const validateObjectId = (_id: string): void => {
+  if (!isValidObjectId(_id)) {
+    throw new CustomError(
+      'Id must have 24 hexadecimal characters',
+      HTTPCodes.BAD_REQUEST,
+    );
+  }
+}
+
+export default validateObjectId;

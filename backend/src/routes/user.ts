d20de49d07d5fb35755934a308e94d1be1d8172d
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 23:43:00 -0300

    feat: classe UserRoute

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,20 @@
+import UserController from '../controllers/user';
+import User from '../models/user';
+import UserService from '../services/user';
+import Route from './route';
+
+class UserRoute extends Route {
+  private _userController: UserController;
+
+  constructor(userController: UserController) {
+    super();
+    this._userController = userController;
+
+  }
+}
+
+const userService: UserService = new UserService(User);
+const userController: UserController = new UserController(userService);
+const userRoute: UserRoute = new UserRoute(userController);
+
+export default userRoute;

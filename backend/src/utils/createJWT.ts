75a533b22ee800189ad8d6e8913378dea3c81315
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 23:01:26 -0300

    fix: createJWT args type

--- backend/src/utils/createJWT.ts
+++ backend/src/utils/createJWT.ts
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
-import IUser from '../interfaces/IUser';
 import 'dotenv/config';
+import UserDocument from '../interfaces/UserDocument';
 
 const JWT_SECRET: string = process.env.JWT_SECRET as string;
 
-const createJWT = (user: IUser): string => {
-  const { username, password } = user;
-  const token: string = jwt.sign({ username, password }, JWT_SECRET, { algorithm: 'HS256' });
+const createJWT = (user: UserDocument): string => {
+  const { _id } = user;
+  const token: string = jwt.sign({ _id }, JWT_SECRET, { algorithm: 'HS256' });
   return token;
 };
 

7ec2135ba2afbcb1671a605285f8bb984cef84e0
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-14 23:42:08 -0300

    feat[UserController]: método delete que deleta usuário

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -9,12 +9,23 @@ class UserController {
     this._userService = userService;
 
     this.create = this.create.bind(this);
+    this.delete = this.delete.bind(this);
   }
 
   public async create(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const { code, data } = await this._userService.create(req.body as IUser);
       res.status(code).json({ token: data });
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+
+  public async delete(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { _id } = req.params;
+      const { code } = await this._userService.delete(_id);
+      res.status(code).end();
     } catch (error: unknown) {
       next(error);
     }

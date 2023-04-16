12eb7f02f50e49976c0f6eef398f8ec53da59acb
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-15 22:33:38 -0300

    fix typing

--- backend/src/utils/validateUser.ts
+++ backend/src/utils/validateUser.ts
@@ -1,7 +1,7 @@
 import HTTPCodes from '../enum/HTTPCodes'
 import CustomError from './CustomError'
 
-const validateUser = (_id: string, userId: string) => {
+const validateUser = (_id: string, userId: string): void => {
   if (_id !== userId) {
     throw new CustomError("not able to do this action.", HTTPCodes.FORBIDDEN);
   }

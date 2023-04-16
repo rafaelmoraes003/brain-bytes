6a8ee92a78c304249b25cb23162172addf79a303
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-15 22:29:38 -0300

    feat: classe Middlewares

---
+++ backend/src/middlewares/middlewares.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import CustomError from '../utils/CustomError';
+import HTTPCodes from '../enum/HTTPCodes';
+import AuthRequest from '../interfaces/AuthRequest';
+import IUser from '../interfaces/IUser';
+
+class Middlewares {
+
+
+}
+
+export default Middlewares;

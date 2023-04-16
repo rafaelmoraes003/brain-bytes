7614dcbe0656a28b82eb43583eeb0a730b43b3db
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-15 22:30:11 -0300

    feat[Middlewares]: implementa método errorHandler

--- backend/src/middlewares/middlewares.ts
+++ backend/src/middlewares/middlewares.ts
@@ -7,7 +7,20 @@ import IUser from '../interfaces/IUser';
 
 class Middlewares {
 
+  public static errorHandler(
+    err: CustomError,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): void {
+    if (!err.statusCode) {
+      err.statusCode = HTTPCodes.SERVER_ERROR;
+    }
 
+    const { message, statusCode } = err;
+
+    res.status(statusCode).json({ error: message });
+  };
 }
 
 export default Middlewares;

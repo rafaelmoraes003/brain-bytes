a65bd7c85afe245d3b0ecf6c725da498e343572b
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-17 14:33:03 -0300

    feat[QuestionController]: método getByCategory, que retorna 5 perguntas de uma categoria da coleção de forma aleatória

--- backend/src/controllers/question.ts
+++ backend/src/controllers/question.ts
@@ -20,6 +20,16 @@ class QuestionController {
     }
   }
 
+  public async getByCategory(req: AuthRequest, res: Response, next: NextFunction) {
+    const { category } = req.params;
+    try {
+      const { code, data } = await this
+        ._questionService.getByCategory(category as string);
+      res.status(code).json(data);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
 }
 
 export default QuestionController;

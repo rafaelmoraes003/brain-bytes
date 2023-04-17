7c7c674b6bc749d22c4e60c21d66fae2e531aeef
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-17 14:22:56 -0300

    feat: adiciona propriedade isAdmin para token jwt

--- backend/src/utils/createJWT.ts
+++ backend/src/utils/createJWT.ts
@@ -5,8 +5,8 @@ import UserDocument from '../interfaces/UserDocument';
 const JWT_SECRET: string = process.env.JWT_SECRET as string;
 
 const createJWT = (user: UserDocument): string => {
-  const { _id } = user;
-  const token: string = jwt.sign({ _id }, JWT_SECRET, { algorithm: 'HS256' });
+  const { _id, isAdmin } = user;
+  const token: string = jwt.sign({ _id, isAdmin }, JWT_SECRET, { algorithm: 'HS256' });
   return token;
 };
 

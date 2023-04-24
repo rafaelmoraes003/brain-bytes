4d517196037001860c1456f8df03b1c8e1d97b32
Rafael Moraes <rafaelsm003@gmail.com>
2023-04-24 16:32:11 -0300

    refactor: createdUsers mock

--- backend/src/tests/mocks/createdUser.ts
+++ backend/src/tests/mocks/createdUser.ts
@@ -2,17 +2,15 @@ import { Types } from 'mongoose';
 import User from '../../models/user';
 import MockCreatedUser from '../../interfaces/MockCreatedUser';
 
-const mockUsers: MockCreatedUser[] = [
-  {
-    _id: new Types.ObjectId(),
-    username: 'yoshi',
-    password: '827ccb0eea8a706c4c34a16891f84e7b',
-    bytes: 10,
-    availableCategories: ['node.js', 'react', 'python'],
-    isAdmin: false,
-  },
-];
+export const mockUser: MockCreatedUser = {
+  _id: new Types.ObjectId(),
+  username: 'yoshi',
+  password: '827ccb0eea8a706c4c34a16891f84e7b',
+  bytes: 10,
+  availableCategories: ['node.js', 'react', 'python'],
+  isAdmin: false,
+};
 
-const createdUsers = mockUsers.map((user) => new User(user));
+const createdUsers = [mockUser].map((user) => new User(user));
 
 export default createdUsers;

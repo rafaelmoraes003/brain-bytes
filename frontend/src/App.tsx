2ff036e7f3b07e2b85692881ff31674b8053301a
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-07 02:19:10 -0300

    feat: implementa rotas da aplicação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
+import Settings from './pages/Settings';
+import Home from './pages/Home';
+import Store from './pages/Store';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <h1>brain bytes</h1>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/create-account" element={<CreateAccount />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+    </Routes>
   );
 }
 

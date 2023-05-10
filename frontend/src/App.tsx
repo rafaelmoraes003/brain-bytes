a7d044bdf314e7e8a36568ed27b71cd419065cf7
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-10 18:38:16 -0300

    feat[App]: ToastContainer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
 import Settings from './pages/Settings';
@@ -8,14 +10,17 @@ import Store from './pages/Store';
 
 function App(): JSX.Element {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/create-account" element={<CreateAccount />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 

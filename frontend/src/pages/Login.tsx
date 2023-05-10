178aa5049316cb7f4706883cad69c25b117fd0b0
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-10 18:42:49 -0300

    feat[Login]: implementa AuthenticationForm

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import AuthenticationForm from '../components/AuthenticationForm';
 
 function Login(): JSX.Element {
-  return <h1>login</h1>;
+  return (
+    <AuthenticationForm
+      heading="Brain Bytes"
+      subHeading="Byte-sized tech trivia for your brain!"
+      endpoint="/login"
+      linkedRoute="/create-account"
+      linkedRouteText="new here? register now!"
+      buttonTitle="Login"
+    />
+  );
 }
 
 export default Login;

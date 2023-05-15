1fb5f41ed992b05f4916bc9f0c8da3bf3cfefcb9
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-14 23:34:09 -0300

    feat: classe User que representa coleção no Mongo

---
+++ backend/src/schemas/user.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { UserDTO } from '../interfaces/UserDTO';
+
+@Schema()
+export class User implements UserDTO {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, default: 10 })
+  bytes: number;
+
+  @Prop({ required: true, default: ['node.js', 'python', 'java'] })
+  availableCategories: string[];
+
+  @Prop({ required: true, default: false })
+  isAdmin: boolean;
+}

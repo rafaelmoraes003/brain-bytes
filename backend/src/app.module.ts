93f2221d9ff4d0b9084edb79b5e17e2a04b5fd65
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-14 23:37:18 -0300

    feat: adiciona LoginModule aos módulos da aplicação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoginModule } from './login/login.module';
 import 'dotenv/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI as string)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    LoginModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

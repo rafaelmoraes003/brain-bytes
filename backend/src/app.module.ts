3aa74d998f8594e44bd00e0aea3f25b7eed73592
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-14 23:31:09 -0300

    feat: implementa mongoose na aplicação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI as string)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

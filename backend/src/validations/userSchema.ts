78b0117c02a5e2cd72b7c44d151e9f8c2073930d
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-15 02:41:17 -0300

    fix: número máximo de caracteres para senha -> 15

--- backend/src/validations/userSchema.ts
+++ backend/src/validations/userSchema.ts
@@ -3,5 +3,5 @@ import { User } from '../schemas/user';
 
 export const userSchema: ZodType<Partial<User>> = z.object({
   username: z.string().min(3).max(12),
-  password: z.string().min(6).max(5),
+  password: z.string().min(6).max(15),
 });

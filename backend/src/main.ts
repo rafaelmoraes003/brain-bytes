78b00f6602f48384c0115e2eeefe2d6c6771e023
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-14 23:25:28 -0300

    feat: porta da aplicação

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const PORT: number = Number(process.env.PORT) || 3001;
+
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  await app.listen(PORT);
 }
 bootstrap();

efb89062d4a3f42dbaad4b71a00599e57e720cbe
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-14 23:48:35 -0300

    feat[Utils]: método validateBody

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,8 +1,21 @@
+import { UnprocessableEntityException } from '@nestjs/common';
 import md5 from 'md5';
+import { SafeParseReturnType, ZodType } from 'zod';
 
 class Utils {
   public static getHash(string: string): string {
     const hash: string = md5(string);
     return hash;
+  }
+
+  public static validateBody(validator: ZodType, body: unknown) {
+    const parsedObject: SafeParseReturnType<ZodType, unknown> = validator.safeParse(body);
+    let errorMessage: string;
+
+    if (!parsedObject.success) {
+      const error = parsedObject.error.issues[0];
+      errorMessage = `${error.path[0]} - ${error.message}`;
+      throw new UnprocessableEntityException(errorMessage);
+    }
   }
 }

aed6d7ca9e478d82e8ee7d09a5cc3d8ac0264070
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-14 23:58:38 -0300

    feat: adiciona módulo user nos imports do módulo principal da aplicação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
+import { UserModule } from './user/user.module';
 import 'dotenv/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
     LoginModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],

a1a303cba4de773afb17244733e771115ed2345a
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-15 00:03:12 -0300

    feat[UserService]: método create

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ServiceResponse } from 'src/interfaces/ServiceResponse';
 import { User, UserDocument } from 'src/schemas/user';
+import { Utils } from 'src/utils/utils';
+import { userSchema } from 'src/validations/userSchema';
 
 @Injectable()
 export class UserService {
@@ -11,5 +14,22 @@ export class UserService {
     this.userModel = userModel;
   }
 
+  public async create(userToCreate: User): Promise<ServiceResponse<UserDocument>> {
+    Utils.validateBody(userSchema, userToCreate);
 
+    const { username, password } = userToCreate;
+
+    const user: UserDocument | null = await this.userModel.findOne({ username });
+
+    if (user) {
+      throw new ConflictException('user already exists.');
+    }
+
+    const encryptedPassword = Utils.getHash(password);
+
+    const newUser: UserDocument = await this.userModel
+      .create({ username, password: encryptedPassword });
+
+    return { data: newUser };
+  }
 }

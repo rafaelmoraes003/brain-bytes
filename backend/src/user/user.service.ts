87bb89ecc7cfdce39286cafbc15c983df8acafa3
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-15 00:01:05 -0300

    feat[UserService]: injeção de model

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user';
 
 @Injectable()
 export class UserService {
+  private userModel: Model<UserDocument>;
 
+  constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
+    this.userModel = userModel;
+  }
 }

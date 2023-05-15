b9eec657a69687e1c780c1836fbd5ad0806531fe
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-15 00:06:00 -0300

    feat[UserController]: método getById -> GET /user/:_id

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/schemas/user';
+import { Types } from 'mongoose';
 
 @Controller('user')
 export class UserController {
@@ -14,6 +15,13 @@ export class UserController {
   @HttpCode(HttpStatus.OK)
   public async create(@Body() user: User) {
     const { data } = await this.userService.create(user);
+    return data;
+  }
+
+  @Get(':_id')
+  @HttpCode(HttpStatus.OK)
+  public async getById(@Param('_id') _id: Types.ObjectId) {
+    const { data } = await this.userService.getById(_id);
     return data;
   }
 }

fb4c05ce7f24c9155cae61f8442edc687f0c3d21
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-17 15:25:47 -0300

    feat[QuestionController]: método getFiveQuestionsFromCategory -> GET /question/:category

--- backend/src/question/question.controller.ts
+++ backend/src/question/question.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { Question } from 'src/schemas/question';
+import { Categories } from 'src/types/Categories';
 
 @Controller('question')
 export class QuestionController {
@@ -12,6 +13,13 @@ export class QuestionController {
 
   public async create(@Body() question: Question) {
     const { data } = await this.questionService.create(question);
+    return data;
+  }
+
+  @Get(':category')
+  @HttpCode(HttpStatus.OK)
+  public async getFiveQuestionsFromCategory(@Param('category') category: Categories) {
+    const { data } = await this.questionService.getFiveQuestionsFromCategory(category);
     return data;
   }
 }

9f71bf86cfce61cf2e49a0cf82e60b4bd7ea8291
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-17 15:12:48 -0300

    feat[QuestionService]: método create

--- backend/src/question/question.service.ts
+++ backend/src/question/question.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Question, QuestionDocument } from '../schemas/question';
+import { Utils } from 'src/utils/utils';
+import { questionSchema } from 'src/validations/questionSchema';
+import { ServiceResponse } from 'src/interfaces/ServiceResponse';
 
 @Injectable()
 export class QuestionService {
@@ -9,5 +12,11 @@ export class QuestionService {
 
   constructor(@InjectModel(Question.name) questionModel: Model<QuestionDocument>) {
     this.questionModel = questionModel;
+  }
+
+  public async create(question: Question): Promise<ServiceResponse<QuestionDocument>> {
+    Utils.validateBody(questionSchema, question);
+    const newQuestion: QuestionDocument = await this.questionModel.create(question);
+    return { data: newQuestion };
   }
 }

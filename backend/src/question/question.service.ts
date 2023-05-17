6cef2d8715e492484a1c3a085392ee642e48f27a
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-17 15:02:52 -0300

    feat: classe QuestionService

---
+++ backend/src/question/question.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Question, QuestionDocument } from 'src/schemas/question';
+
+@Injectable()
+export class QuestionService {
+  private questionModel: Model<QuestionDocument>;
+
+  constructor(@InjectModel(Question.name) questionModel: Model<QuestionDocument>) {
+    this.questionModel = questionModel;
+  }
+}

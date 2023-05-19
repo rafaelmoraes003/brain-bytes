2336b85698d895bed31bee6a9ef749bb5608015d
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-19 00:48:59 -0300

    feat[Home]: renderização de categorias

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import categories from '../assets/categories';
 
 function Home(): JSX.Element {
-  return <h1>home</h1>;
+  return (
+    <div>
+      {categories.map((category) => (
+        <div
+          style={{
+            backgroundColor: category.backgroundColor,
+            width: 300,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: 15,
+            padding: 10,
+            borderRadius: 8,
+            fontFamily: 'Lexend Deca',
+          }}
+        >
+          <img src={category.uri} alt={category.name} />
+          <h2>{category.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;

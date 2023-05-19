83cf447984c1c994121f4eb7c87d185ba1e63c97
Rafael Moraes <rafaelsm003@gmail.com>
2023-05-19 00:47:10 -0300

    feat: habilita cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ const PORT: number = Number(process.env.PORT) || 3001;
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(PORT);
 }
 bootstrap();
